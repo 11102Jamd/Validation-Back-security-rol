778404592518739aab48dfb5bb5fe13cd2c23723
11102Jamd <jalejandromd17@gmail.com>
2025-05-18 21:25:46 -0500

    Se agregan metodos que facilitan el mantenimiento del codigo y aumentan la seguridad utilizando el encapsulamiento

--- app/Models/Manufacturing/Recipe.php
+++ app/Models/Manufacturing/Recipe.php
@@ -28,23 +28,39 @@ class Recipe extends Model
         return $this->belongsTo(Manufacturing::class, 'ID_manufacturing');
     }
 
+    /**
+     * Laravel convierte estos metodos en propiedades dinamicas como en la linea 57
+     */
     public function input(): BelongsTo
     {
         return $this->belongsTo(Input::class, 'ID_inputs');
     }
 
+    /**
+     * Metodo privado que busca el ultio precio por referencia del insumo
+     */
+    private function getPriceReference()
+    {
+        return $this->input->inputOrders()
+            ->with(['purchaseOrder' => function ($query) {
+                $query->orderBy('PurchaseOrderDate', 'desc');
+            }])
+            ->first();
+    }
+
+
     public function priceQuantitySpent()
     {
+        /**
+         * laravel invoca automaticamente el metodo input() y decuelve el metodo relacionado
+         */
         $input = $this->input;
 
         if (!$input) {
             throw new \Exception("El insumo asociado no exsite");
         }
 
-        $inputOrder = $this->input->inputOrders()->with(['purchaseOrder' => function($query) {
-            $query->orderBy('PurchaseOrderDate', 'desc');
-        }])
-        ->first();
+        $inputOrder = $this->getPriceReference();
 
         if (!$inputOrder) {
             throw new \Exception("No se encontro el insumo asociado con su orden de compra");
@@ -59,7 +75,6 @@ class Recipe extends Model
         }
 
         return round($amountInOrderUnit * $inputOrder->UnityPrice, 2);
-
     }
 
     public function RestoreStockInputs()
@@ -70,5 +85,19 @@ class Recipe extends Model
             $input->increment('CurrentStock', $this->AmountSpent);
         }
     }
-}
 
+    // protected function convertToOrderUnit(float $amountInGrams, string $orderUnit): float
+    // {
+    //     switch ($orderUnit) {
+    //         case 'Kg':
+    //             return $amountInGrams / 1000;
+    //         case 'lb':
+    //             return $amountInGrams / 453.592;
+    //         default: // gramos
+    //             return $amountInGrams;
+    //     }
+    // }
+
+    // $amountInOrderUnit = $this->convertToOrderUnit($this->AmountSpent, $inputOrder->UnitMeasurement);
+    // esta linea anterior va en el metodo pricequantitySpent
+}
